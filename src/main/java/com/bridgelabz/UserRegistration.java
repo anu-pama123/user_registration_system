714e87409121c7e15b3ffb18706f751cc5b5473b
anupama cv <anu.anupamacv@gmail.com>
2021-05-30 22:00:50 +0530

    ADDED: functionality to validate first name

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,6 +1,15 @@
 package com.bridgelabz;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration
 {
-
+    private final String FIRST_NAME_PATTERN ="^[A-Z]+[a-z]{2,}$";
+    public boolean validateFirstName(String firstName)
+    {
+        Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.find();
+    }
 }
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -1,6 +1,16 @@
 package com.bridgelabz;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class UserRegistrationTest
 {
+    @Test
+    public void givenFirstName_whenProper_returnTrue()
+    {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean status = userRegistration.validateFirstName("Anupama");
+        Assertions.assertEquals(status, true);
+    }
 
 }
